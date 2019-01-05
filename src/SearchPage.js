41710636d33f050cc6cf5dab9eeeaded1a04a2a4
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2019-01-06 00:01:47 +0100

    feat(client): Add Search form, Add display Result

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,129 +1,135 @@
 import React, { Component } from 'react';
 import ItineraryBox from './ItineraryBox';
+import { Redirect } from 'react-router';
 
 class SearchPage extends Component {
     render() {
-        const data = {
-            "Depart": {
-                "Direct": false,
-                "Path": [
-                    {
-                        "Origin": {
-                            "Id": 10413,
-                            "ParentId": 6073,
-                            "Code": "CDG",
-                            "Type": "Airport",
-                            "Name": "Paris Charles de Gaulle"
-                        },
-                        "Destination": {
-                            "Id": 13554,
-                            "ParentId": 4698,
-                            "Code": "LHR",
-                            "Type": "Airport",
-                            "Name": "London Heathrow"
-                        },
-                        "Departure": "2019-01-06T10:25:00",
-                        "Arrival": "2019-01-06T10:50:00",
-                        "Duration": 85,
-                        "Carrier": {
-                            "Id": 881,
-                            "Code": "BA",
-                            "Name": "British Airways",
-                            "ImageUrl": "https://s1.apideeplink.com/images/airlines/BA.png",
-                            "DisplayCode": "BA"
-                        }
-                    },
-                    {
-                        "Origin": {
-                            "Id": 13554,
-                            "ParentId": 4698,
-                            "Code": "LHR",
-                            "Type": "Airport",
-                            "Name": "London Heathrow"
-                        },
-                        "Destination": {
-                            "Id": 12712,
-                            "ParentId": 5772,
-                            "Code": "JFK",
-                            "Type": "Airport",
-                            "Name": "New York John F. Kennedy"
-                        },
-                        "Departure": "2019-01-06T12:55:00",
-                        "Arrival": "2019-01-06T16:10:00",
-                        "Duration": 495,
-                        "Carrier": {
-                            "Id": 881,
-                            "Code": "BA",
-                            "Name": "British Airways",
-                            "ImageUrl": "https://s1.apideeplink.com/images/airlines/BA.png",
-                            "DisplayCode": "BA"
-                        }
-                    }
-                ],
-                "TotalDuration": 705
-            },
-            "Return": {
-                "Direct": false,
-                "Path": [
-                    {
-                        "Origin": {
-                            "Id": 12712,
-                            "ParentId": 5772,
-                            "Code": "JFK",
-                            "Type": "Airport",
-                            "Name": "New York John F. Kennedy"
-                        },
-                        "Destination": {
-                            "Id": 13554,
-                            "ParentId": 4698,
-                            "Code": "LHR",
-                            "Type": "Airport",
-                            "Name": "London Heathrow"
-                        },
-                        "Departure": "2019-01-11T22:29:00",
-                        "Arrival": "2019-01-12T10:30:00",
-                        "Duration": 421,
-                        "Carrier": {
-                            "Id": 881,
-                            "Code": "BA",
-                            "Name": "British Airways",
-                            "ImageUrl": "https://s1.apideeplink.com/images/airlines/BA.png",
-                            "DisplayCode": "BA"
-                        }
-                    },
-                    {
-                        "Origin": {
-                            "Id": 13554,
-                            "ParentId": 4698,
-                            "Code": "LHR",
-                            "Type": "Airport",
-                            "Name": "London Heathrow"
-                        },
-                        "Destination": {
-                            "Id": 10413,
-                            "ParentId": 6073,
-                            "Code": "CDG",
-                            "Type": "Airport",
-                            "Name": "Paris Charles de Gaulle"
-                        },
-                        "Departure": "2019-01-12T13:05:00",
-                        "Arrival": "2019-01-12T15:20:00",
-                        "Duration": 75,
-                        "Carrier": {
-                            "Id": 881,
-                            "Code": "BA",
-                            "Name": "British Airways",
-                            "ImageUrl": "https://s1.apideeplink.com/images/airlines/BA.png",
-                            "DisplayCode": "BA"
-                        }
-                    }
-                ],
-                "TotalDuration": 651
-            },
-            "Price": 2801.33
-        };
-        const dataList = [data, data];
-        const boxes = dataList.map((data, index) => <ItineraryBox Itinerary={data} key={index} />)
+        if (!this.props.location.state) {
+            return (
+                <Redirect to='/' />
+            )
+        }
+        // const data = {
+        //     "Depart": {
+        //         "Direct": false,
+        //         "Path": [
+        //             {
+        //                 "Origin": {
+        //                     "Id": 10413,
+        //                     "ParentId": 6073,
+        //                     "Code": "CDG",
+        //                     "Type": "Airport",
+        //                     "Name": "Paris Charles de Gaulle"
+        //                 },
+        //                 "Destination": {
+        //                     "Id": 13554,
+        //                     "ParentId": 4698,
+        //                     "Code": "LHR",
+        //                     "Type": "Airport",
+        //                     "Name": "London Heathrow"
+        //                 },
+        //                 "Departure": "2019-01-06T10:25:00",
+        //                 "Arrival": "2019-01-06T10:50:00",
+        //                 "Duration": 85,
+        //                 "Carrier": {
+        //                     "Id": 881,
+        //                     "Code": "BA",
+        //                     "Name": "British Airways",
+        //                     "ImageUrl": "https://s1.apideeplink.com/images/airlines/BA.png",
+        //                     "DisplayCode": "BA"
+        //                 }
+        //             },
+        //             {
+        //                 "Origin": {
+        //                     "Id": 13554,
+        //                     "ParentId": 4698,
+        //                     "Code": "LHR",
+        //                     "Type": "Airport",
+        //                     "Name": "London Heathrow"
+        //                 },
+        //                 "Destination": {
+        //                     "Id": 12712,
+        //                     "ParentId": 5772,
+        //                     "Code": "JFK",
+        //                     "Type": "Airport",
+        //                     "Name": "New York John F. Kennedy"
+        //                 },
+        //                 "Departure": "2019-01-06T12:55:00",
+        //                 "Arrival": "2019-01-06T16:10:00",
+        //                 "Duration": 495,
+        //                 "Carrier": {
+        //                     "Id": 881,
+        //                     "Code": "BA",
+        //                     "Name": "British Airways",
+        //                     "ImageUrl": "https://s1.apideeplink.com/images/airlines/BA.png",
+        //                     "DisplayCode": "BA"
+        //                 }
+        //             }
+        //         ],
+        //         "TotalDuration": 705
+        //     },
+        //     "Return": {
+        //         "Direct": false,
+        //         "Path": [
+        //             {
+        //                 "Origin": {
+        //                     "Id": 12712,
+        //                     "ParentId": 5772,
+        //                     "Code": "JFK",
+        //                     "Type": "Airport",
+        //                     "Name": "New York John F. Kennedy"
+        //                 },
+        //                 "Destination": {
+        //                     "Id": 13554,
+        //                     "ParentId": 4698,
+        //                     "Code": "LHR",
+        //                     "Type": "Airport",
+        //                     "Name": "London Heathrow"
+        //                 },
+        //                 "Departure": "2019-01-11T22:29:00",
+        //                 "Arrival": "2019-01-12T10:30:00",
+        //                 "Duration": 421,
+        //                 "Carrier": {
+        //                     "Id": 881,
+        //                     "Code": "BA",
+        //                     "Name": "British Airways",
+        //                     "ImageUrl": "https://s1.apideeplink.com/images/airlines/BA.png",
+        //                     "DisplayCode": "BA"
+        //                 }
+        //             },
+        //             {
+        //                 "Origin": {
+        //                     "Id": 13554,
+        //                     "ParentId": 4698,
+        //                     "Code": "LHR",
+        //                     "Type": "Airport",
+        //                     "Name": "London Heathrow"
+        //                 },
+        //                 "Destination": {
+        //                     "Id": 10413,
+        //                     "ParentId": 6073,
+        //                     "Code": "CDG",
+        //                     "Type": "Airport",
+        //                     "Name": "Paris Charles de Gaulle"
+        //                 },
+        //                 "Departure": "2019-01-12T13:05:00",
+        //                 "Arrival": "2019-01-12T15:20:00",
+        //                 "Duration": 75,
+        //                 "Carrier": {
+        //                     "Id": 881,
+        //                     "Code": "BA",
+        //                     "Name": "British Airways",
+        //                     "ImageUrl": "https://s1.apideeplink.com/images/airlines/BA.png",
+        //                     "DisplayCode": "BA"
+        //                 }
+        //             }
+        //         ],
+        //         "TotalDuration": 651
+        //     },
+        //     "Price": 2801.33
+        // };
+        // const dataList = [data, data];
+        const boxes = this.props.location.state.itineraries.map((data, index) => <ItineraryBox Itinerary={data} key={index} />)
         return (
             <section className="section">
                 <div className="container">
--- src/SearchSection.js
+++ src/SearchSection.js
@@ -1,6 +1,50 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import moment from 'moment';
+import axios from 'axios';
 
 class SearchSection extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchQuery: {
+                "country": "UK",
+                "currency": "GBP",
+                "locale": "en-GB",
+                "originPlace": "CDG-sky",
+                "destinationPlace": "JFK-sky",
+                "outboundDate": moment().format('YYYY-MM-DD'),
+                "inboundDate": moment().add(7, 'days').format('YYYY-MM-DD'),
+                "cabinClass": "economy",
+                "adults": "1",
+                "children": "0",
+                "infants": "0",
+                "groupPricing": true,
+            }
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        let searchQuery = {
+            ...this.state.searchQuery,
+        };
+        searchQuery[event.target.name] = event.target.value;
+        this.setState({ searchQuery });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        axios.post('/api/createSession', this.state.searchQuery)
+            .then(res => axios.get(`/api/searchFlight?sessionKey=${res.data}`))
+            .then(res => this.props.history.push({
+                pathname: '/search',
+                state: {itineraries: res.data.Itineraries}
+            }))
+            .catch(err => console.log(err.response));
+    }
+
     render() {
         return (
             <section className="section is-medium">
@@ -9,51 +53,51 @@ class SearchSection extends Component {
                     <div className="columns">
                         <div className="column is-one-quarter">
                             <div className="field">
-                                <label className="label">Inbound Date</label>
+                                <label className="label">Outbound Date</label>
                                 <div className="control">
-                                    <input type="date" className="input" />
+                                    <input type="date" name="outboundDate" className="input" value={this.state.searchQuery.outboundDate} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="field">
-                                <label className="label">Outbound Date</label>
+                                <label className="label">Inbound Date</label>
                                 <div className="control">
-                                    <input type="date" className="input" />
+                                    <input type="date" name="inboundDate" className="input" value={this.state.searchQuery.inboundDate} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="field">
                                 <label className="label">From</label>
                                 <div className="control">
-                                    <input type="input" className="input" />
+                                    <input type="input" name="originPlace" className="input" value={this.state.searchQuery.originPlace} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="field">
                                 <label className="label">To</label>
                                 <div className="control">
-                                    <input type="input" className="input" />
+                                    <input type="input" name="destinationPlace" className="input" value={this.state.searchQuery.destinationPlace} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="field">
                                 <label className="label">Travel class</label>
                                 <div className="control">
-                                    <input type="input" className="input" />
+                                    <input type="input" name="cabinClass" className="input" value={this.state.searchQuery.cabinClass} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="field">
                                 <label className="label">Adults</label>
                                 <div className="control">
-                                    <input type="number" className="input" />
+                                    <input type="number" name="adults" className="input" value={this.state.searchQuery.adults} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="field">
-                                <label className="label">Teenagers</label>
+                                <label className="label">Children (4 - 18)</label>
                                 <div className="control">
-                                    <input type="number" className="input" />
+                                    <input type="number" name="children" className="input" value={this.state.searchQuery.children} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="field">
-                                <label className="label">Infants</label>
+                                <label className="label">Infants(0 - 3)</label>
                                 <div className="control">
-                                    <input type="number" className="input" />
+                                    <input type="number" name="infants" className="input" value={this.state.searchQuery.infants} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="field">
@@ -69,4 +113,4 @@ class SearchSection extends Component {
     }
 };
 
-export default SearchSection;
+export default withRouter(SearchSection);
